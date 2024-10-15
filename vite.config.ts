5e75207d6f3f2e361d27918b892f25b36bbcad5b
Hai.Nguyen <haingdc@gmail.com>
2024-10-15 09:27:05 +0700

    [Deno Deploy] Refactor Vite config for dynamic API URL based on environment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+let url =  "http://localhost:8000"
+const isDev = false
+
+if (!isDev) {
+  url = "https://pedals-analyze-server.deno.dev"
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: url,
         changeOrigin: true,
       },
     },
