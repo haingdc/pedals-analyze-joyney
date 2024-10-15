44fd6f0179e99ec0ab722c05454e6b47bd1ff752
Hai.Nguyen <haingdc@gmail.com>
2024-10-15 09:29:03 +0700

    [Deno Deploy] Revert dynamic API URL logic in Vite config, resetting to localhost for development environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-let url =  "http://localhost:8000"
-const isDev = false
-
-if (!isDev) {
-  url = "https://pedals-analyze-server.deno.dev"
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: url,
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
     },
